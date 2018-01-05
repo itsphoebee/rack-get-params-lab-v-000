ca79dafb067ac72ea04f3f27c86606225a4d2905
Phoebe Lau <phoebelau7@gmail.com>
2018-01-05 19:09:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -42,9 +42,9 @@ class Application
   def add_item(search_term)
     if @@items.include?(search_term)
       @@cart << search_term
-      resp.write "added #{search_term}"
+      return "added #{search_term}"
     else
-      resp.write "Sorry! We don't have that item."
+      return "Sorry! We don't have that item."
     end
   end
 end
