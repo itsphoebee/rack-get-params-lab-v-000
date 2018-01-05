9bb0758ce821d626dcc55aa7364123295f2b31f3
Phoebe Lau <phoebelau7@gmail.com>
2018-01-05 19:01:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,13 @@ class Application
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      if @@items.include?(search_term)
+        @@cart << search_term
+      else
+        resp.write "Sorry! We do not have this item."
+      end
     else
       resp.write "Path Not Found"
     end
